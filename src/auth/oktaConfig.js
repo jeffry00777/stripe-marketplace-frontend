2ac45888e52f00cb49ddfead40030fbd1ec155c3
Jeffry Livingston <50872197+jeffry00777@users.noreply.github.com>
2025-07-29 13:33:27 -0400

    Update oktaConfig.js

--- src/auth/oktaConfig.js
+++ src/auth/oktaConfig.js
@@ -2,7 +2,7 @@
 export const oktaConfig = {
   clientId: "0oat7m5qecol7ATfb697",
   issuer: "https://trial-7472583.okta.com/oauth2/default",
-  redirectUri: window.location.origin + "/login/callback",
+  redirectUri: "https://stripe-marketplace-frontend.vercel.app/login/callback",
   scopes: ["openid", "profile", "email"],
   pkce: true,
 };
